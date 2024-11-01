5d415e0c166e9d895d3899d2a10010a341116415
TinyMeng <666@majiameng.com>
2024-11-01 18:34:43 +0800

    first commit

--- src/Gateways/Meeting.php
+++ src/Gateways/Meeting.php
@@ -1,15 +1,6 @@
 <?php
 /**
- * 网站应用微信登录开发 https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=open1419316505&token=
- * 1.PC登录:微信开放平台创建'网站应用'
- * 2.Mobile登录:微信公众号(服务号/企业号)
- * 3.APP登录:微信开放平台创建'移动应用'
- *
- * 注: scope值
- *      1、以snsapi_base为scope发起的网页授权，是用来获取进入页面的用户的openid的，并且是静默授权并自动跳转到回调页的。用户感知的就是直接进入了回调页（往往是业务页面）
- *      2、以snsapi_userinfo为scope发起的网页授权，是用来获取用户的基本信息的。但这种授权需要用户手动同意，并且由于用户同意过，所以无须关注，就可在授权后获取该用户的基本信息。(H5页面微信授权获取用户,注册成为用户id,可以做点赞关注等功能)
- *      3、用户管理类接口中的“获取用户基本信息接口”，是在用户和公众号产生消息交互或关注后事件推送后，才能根据用户OpenID来获取用户基本信息。这个接口，包括其他微信接口，都是需要该用户（即openid）关注了公众号后，才能调用成功的。
- * 如想打通unionid的话需要将公众号绑定到同一个微信开放平台
+ * https://cloud.tencent.com/document/product/1095/42417
  */
 namespace Tinymeng\Tencent\Meeting\Gateways;
 
