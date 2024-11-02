318c377bf0fbbda4244830f951b91ee15bcb0967
tinymeng <666@majiameng.com>
2024-11-02 16:41:38 +0800

    update

--- example/meeting.php
+++ example/meeting.php
@@ -1,8 +1,20 @@
 <?php
 
-use Tinymeng\Tencent\Meeting\Meeting;
+use tinymeng\Tencent\Meeting\Meeting;
 
 require_once "../vendor/autoload.php";
+use TencentCloud\Cvm\V20170312\CvmClient;
+use TencentCloud\Cvm\V20170312\Models\DescribeInstancesRequest;
+use TencentCloud\Common\Exception\TencentCloudSDKException;
+use TencentCloud\Common\Credential;
 
-$meeting = Meeting::meeting([]);
-var_dump($meeting);
+try {
+    $cred = new Credential("secretId", "secretKey");
+    $client = new CvmClient($cred, "ap-guangzhou");
+    $req = new DescribeInstancesRequest();
+    $resp = $client->DescribeInstances($req);
+    print_r($resp->toJsonString());
+}
+catch(TencentCloudSDKException $e) {
+    echo $e;
+}
